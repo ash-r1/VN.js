8f15b54e04e2ca8275b20362c8338cbca7916abd
Ash <ash.suzuki.r1@gmail.com>
2020-05-16 02:50:35 +0900

    PIXI.js example

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
-function component() {
-  const element = document.createElement('div');
+import * as PIXI from 'pixi.js';
 
-  element.innerHTML = 'Hello, World';
+const app = new PIXI.Application();
 
-  return element;
-}
+// The application will create a canvas element for you that you
+// can then insert into the DOM.
+document.body.appendChild(app.view);
 
-document.body.appendChild(component());
+// load the texture we need
+app.loader.add('bunny', 'bunny.png').load((loader, resources) => {
+  // This creates a texture from a 'bunny.png' image.
+  const bunny = new PIXI.Sprite(resources.bunny?.texture);
+
+  // Setup the position of the bunny
+  bunny.x = app.renderer.width / 2;
+  bunny.y = app.renderer.height / 2;
+
+  // Rotate around the center
+  bunny.anchor.x = 0.5;
+  bunny.anchor.y = 0.5;
+
+  // Add the bunny to the scene we are building.
+  app.stage.addChild(bunny);
+
+  // Listen for frame updates
+  app.ticker.add(() => {
+    // each frame we spin the bunny around a bit
+    bunny.rotation += 0.01;
+  });
+});
