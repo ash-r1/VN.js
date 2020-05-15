b06b990f14daac3bf0e0152a0277beba820cb5fc
Ash <ash.suzuki.r1@gmail.com>
2020-05-16 02:09:58 +0900

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'simple-import-sort',
+    'prettier',
+  ],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+      },
+    ],
+    'simple-import-sort/sort': [
+      'warn',
+      {
+        groups: [
+          // React first
+          ['^react', '^@?\\w'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Packages.
+          // pixijs
+          ['^@pixjs'],
+          // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+          ['^@?\\w'],
+          // Absolute imports and other imports such as Vue-style `@/foo`.
+          // Anything that does not start with a dot.
+          ['^[^.]', '^(src)(/.*|$)'],
+          // internal imports.
+          ['^(src)(/.*|$)'],
+          // Relative imports.
+          // Anything that starts with a dot.
+          ['^\\.'],
+        ],
+      },
+    ],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
+};
