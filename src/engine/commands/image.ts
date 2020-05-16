493e15251ab5a0006d16b69d2429ec250bc8d4c8
Ash <ash.suzuki.r1@gmail.com>
2020-05-16 23:08:25 +0900

    add tickPromise to handle ticker based animation

--- src/engine/commands/image.ts
+++ src/engine/commands/image.ts
@@ -8,48 +8,49 @@ export interface ShowOption {
   y?: number;
 }
 
+// tick promise
+function tickPromise(
+  ticker: PIXI.Ticker,
+  duration: number,
+  update: (ratio: number) => void
+): Promise<void> {
+  return new Promise((resolve) => {
+    let t = 0;
+    const tick = (delta: number) => {
+      const ms = delta * ticker.deltaMS;
+      // この計算そもそも合ってるの？
+      t += ms;
+
+      // finalize if duration reaches target duration
+      if (t >= duration) {
+        ticker.remove(tick);
+        update(1.0);
+        resolve();
+        return;
+      }
+
+      update(t / duration);
+    };
+    ticker.add(tick);
+
+    // TODO: easing func?
+  });
+}
+
 export default class Image {
   constructor(private r: Renderer) {}
 
   // TODO: tickerを元にしたアニメーションの良い感じのPromiseがほしい
 
-  fadeIn(name: string, duration: number): Promise<void> {
-    return new Promise((resolve) => {
-      let t = 0;
-      const update = (delta: number) => {
-        const ms = delta * this.r.ticker.deltaMS;
-        t += ms;
-
-        const alpha = Math.min(1.0, t / duration);
-        this.r.SetLayerProps(name, { alpha });
-
-        if (t >= duration) {
-          this.r.ticker.remove(update);
-          resolve();
-        }
-      };
-      this.r.ticker.add(update);
-      // TODO: easing func?
+  private async fadeIn(name: string, duration: number): Promise<void> {
+    await tickPromise(this.r.ticker, duration, (ratio) => {
+      this.r.SetLayerProps(name, { alpha: ratio });
     });
   }
 
-  fadeOut(name: string, duration: number): Promise<void> {
-    return new Promise((resolve) => {
-      let t = 0;
-      const update = (delta: number) => {
-        const ms = delta * this.r.ticker.deltaMS;
-        t += ms;
-
-        const alpha = Math.max(0.0, 1 - t / duration);
-        this.r.SetLayerProps(name, { alpha });
-
-        if (t >= duration) {
-          this.r.ticker.remove(update);
-          resolve();
-        }
-      };
-      this.r.ticker.add(update);
-      // TODO: easing func?
+  private async fadeOut(name: string, duration: number): Promise<void> {
+    await tickPromise(this.r.ticker, duration, (ratio) => {
+      this.r.SetLayerProps(name, { alpha: 1 - ratio });
     });
   }
 
