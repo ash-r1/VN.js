bacd0e4ef900743e8c14b51a7da8b950f556a00a
Ash <ash.suzuki.r1@gmail.com>
2020-05-24 14:58:04 +0900

    rename "middle" of blink to "ajar"

--- src/engine/layer/BlinkAnimationSprite.ts
+++ src/engine/layer/BlinkAnimationSprite.ts
@@ -2,7 +2,7 @@ import * as PIXI from 'pixi.js';
 
 const blinkFrames = (
   normal: PIXI.Texture,
-  middle: PIXI.Texture,
+  ajar: PIXI.Texture,
   close: PIXI.Texture,
   ratio: number
 ): PIXI.AnimatedSprite.FrameObject[] => {
@@ -10,22 +10,22 @@ const blinkFrames = (
     // 最初はすぐまばたき
     [normal, 2.0 * ratio],
     [close, 0.1],
-    [middle, 0.067],
+    [ajar, 0.067],
     // しつこいと鬱陶しいので次は長くとる
     [normal, 8.0 * ratio],
     [close, 0.1],
-    [middle, 0.067],
+    [ajar, 0.067],
     // また長く取って二連瞬き
     [normal, 2.0 * ratio],
     [close, 0.067],
-    [middle, 0.033],
+    [ajar, 0.033],
     [normal, 0.067],
     [close, 0.1],
-    [middle, 0.067],
+    [ajar, 0.067],
     // しつこいと鬱陶しいのでさらに長くとる
     [normal, 12.0],
     [close, 0.1],
-    [middle, 0.067],
+    [ajar, 0.067],
   ] as [PIXI.Texture, number][]).map(([texture, time]) => ({
     texture,
     time: time * 1000,
@@ -36,10 +36,10 @@ export default class BlinkAnimationSprite extends PIXI.AnimatedSprite {
   //
   constructor(
     normal: PIXI.Texture,
-    middle: PIXI.Texture,
+    ajar: PIXI.Texture,
     close: PIXI.Texture,
     ratio = 1.0
   ) {
-    super(blinkFrames(normal, middle, close, ratio));
+    super(blinkFrames(normal, ajar, close, ratio));
   }
 }
