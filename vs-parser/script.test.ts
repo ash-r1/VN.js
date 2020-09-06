0b8b6326281937a419971831298edd695323099a
Ash <ash.suzuki.r1@gmail.com>
2020-09-06 15:22:09 +0900

    add real testcase

--- vs-parser/script.test.ts
+++ vs-parser/script.test.ts
@@ -1,3 +1,10 @@
+import { Script } from './script';
+
 test('adds 1 + 2 to equal 3', () => {
   expect(1 + 2).toBe(3);
+});
+
+test('script parse', () => {
+  const script = Script.parse('*test');
+  expect(script.statements.length).toBe(1);
 });
