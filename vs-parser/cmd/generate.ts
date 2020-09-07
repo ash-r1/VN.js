7e5785a2bb3e5c47f5b106c66b3dc54611a4c719
Ash <ash.suzuki.r1@gmail.com>
2020-09-07 12:36:30 +0900

    change generate.ts options

--- vs-parser/cmd/generate.ts
+++ vs-parser/cmd/generate.ts
@@ -4,22 +4,23 @@ import path from 'path';
 import { Generator } from '../generator';
 import { Script } from '../script';
 
-if (process.argv.length <= 2) {
-  console.error('USAGE: generate.ts your-scenario.vs');
+if (process.argv.length <= 3) {
+  console.error(
+    'USAGE: generate.ts src/scenario/your-scenario.vs src/scenario/your-scenario.ts'
+  );
   process.exit(1);
 }
 
-const fileName = process.argv[2];
-const filePath = path.parse(fileName);
+const src = process.argv[2];
+const dst = process.argv[3];
 
-const body = fs.readFileSync(fileName, 'utf-8');
+const body = fs.readFileSync(src, 'utf-8');
 
 try {
   const script = Script.parse(body);
   const g = new Generator();
-  const ts = g.run(script, filePath.name);
-  const tsPath = path.join(filePath.dir, `${filePath.name}.ts`);
-  fs.writeFileSync(tsPath, ts);
+  const ts = g.run(script, path.parse(src).name);
+  fs.writeFileSync(dst, ts);
 } catch (e) {
   console.error(e.message);
   process.exit(1);
